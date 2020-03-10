80ab4479c8a243e537ccd526698a8a63dcfd1995
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-10 13:55:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,6 +22,9 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  arr = [
+    { :name => "Don Gately", :occupation => "Live-in Staff" },
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
